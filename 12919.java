d190a14050017dc102063d9a3d6a8cfbf44daa40
choibeomjun99 <choibj0414@gmail.com>
2023-09-29 23:52:03 +0900

    complete

---
+++ 12919.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int tmp = 0;
+        
+        for (int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                tmp = i;
+                break;
+            }
+            
+        }
+        
+        return "김서방은 " + Integer.toString(tmp) + "에 있다";
+    }
+}
