f660e0828e2a05677e3218b055259a41ac06438e
choibeomjun99 <choibj0414@gmail.com>
2024-07-10 10:19:18 +0900

    그리디(4)-투포인터

--- python/42885.py
+++ python/42885.py
@@ -17,3 +17,20 @@ def solution(people, limit):
         answer += 1
     
     return answer
+
+#2. 투포인터로 풀어보기
+def solution(people, limit):
+    people.sort()
+    twoPeople = 0 # 두명이 한 번에 건너는 상황
+     
+    light = 0
+    heavy = len(people) - 1
+    
+    while light < heavy: #투포인터 인덱스는 서로 같아지는 시점 직전에 종료
+        if people[light] + people[heavy] <= limit:
+            twoPeople += 1
+            light += 1
+        
+        heavy -= 1
+        
+    return len(people) - twoPeople
