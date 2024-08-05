84218926efd5bceb33b57adcb288b9afb115d069
choibeomjun99 <choibj0414@gmail.com>
2024-08-05 22:15:15 +0900

    2559백준

--- python/baekjoon/2559.py
+++ python/baekjoon/2559.py
@@ -1,22 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-result =[]
-
-n, k = map(int,input().split()) # 
+n, k = map(int,input().split()) #N: 온도를 잰 날들의 개수, k:더해야하는 연속일 수 
 a = list(map(int, input().split()))
 
-answer = sum(a[0:k])
-prev = answer
+now = sum(a[0:k])
+max = now
 
-for i in range(1, n-k+1):
-    tmp = prev - a[i-1] + a[i+k-1]
+for i in range(1, len(a) - k + 1):
+    tmp = now - a[i-1] + a[i+k-1]
+    if max < tmp:
+        max = tmp
+        
+    now = tmp
     
-    if tmp > answer:
-        answer = tmp
-        prev = tmp
-        
-    else:
-        prev = tmp
-        
-print(answer)
+
+print(max)
